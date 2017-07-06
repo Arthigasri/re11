98f10cd1790337377a7c9f6614819b108e503583
Arthigasri <arthi1461997@gmail.com>
2017-07-06 22:13:32 +0530

    Create Mutliplication.java

---
+++ Mutliplication.java
@@ -0,0 +1,16 @@
+# re11
+ import java.io.*;
+         import java.util.*;
+         public class Mutliplication
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int a,i;
+    a=sc.nextInt();
+    for(i=1;i<=10;i++)
+    System.out.println(a+"*"+i+"="+a*i);
+  }
+}
